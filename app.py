25fcbbc742af9ff24ca34dd41ac27df24855928e
msameert <msameert123@gmail.com>
2026-04-11 16:32:45 +0500

    supabase connected

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask,render_template, redirect 
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+conn = engine.connect()
 
 app = Flask(__name__)
 
 
-@app.route("/") 
-def home() :
-  return "Hello"
+@app.route("/")
+def home () :
+  return render_template("home.html")
 
+@app.route("/test")
+def test() :
+  return render_template("home.html", message="Supabase connected successfully")
 
 
 if __name__ == "__main__" :
