06ac7ee0e3c27d6c61965ab9442ff44654979657
msameert <msameert123@gmail.com>
2026-04-13 20:27:20 +0500

    Authentication

--- app.py
+++ app.py
@@ -1,21 +1,72 @@
-from flask import Flask,render_template, redirect 
+from flask import Flask,render_template, redirect, session, url_for, request
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 import os
 
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+from backend.models.user import User
+
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
-conn = engine.connect()
-
 app = Flask(__name__)
+
+app.secret_key = os.getenv('SECRET_KEY', 'secret123')
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
 
 
 @app.route("/")
 def home () :
-  return render_template("home.html")
+  if "username" in session :
+    return redirect(url_for("dashboard"))
+  return render_template("signin.html")
+  
+# Login
+@app.route("/login", methods=["POST"])
+def login() :
+             # Collect info from DB 
+      username = request.form['username']
+      password = request.form['password']
+      user = User.query.filter_by(username=username).first()
+      if user and user.check_password(password):
+          session['username'] = username 
+          return redirect(url_for("dashboard"))
+      else :
+          return render_template("signin.html")
+      
+
+# Register
+@app.route("/register", methods=["POST"])
+def register() :
+            # Add a new user in DB
+      username = request.form['username']  
+      password = request.form['password']
+      user = User.query.filter_by(username=username).first()
+      if user :
+          return render_template("signin.html")     # If user already present then return to homepage
+      else :
+          new_user = User(username = username)
+          new_user.set_password(password)
+          db.session.add(new_user)
+          db.session.commit()
+          session['username'] = username
+          return redirect(url_for("dashboard"))
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+      
+@app.route("/dashboard")
+def dashboard():
+    if "username" in session:
+        return render_template("dashboard.html", username=session['username'])
+    return redirect(url_for("home"))
 
 @app.route("/test")
 def test() :
--- backend/models/user.py
+++ backend/models/user.py
@@ -1,19 +1,16 @@
-from sqlalchemy import Column, BigInteger, String, Text
-from sqlalchemy.orm import declarative_base
+from db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
-Base = declarative_base()
-
-class User(Base):
+class User(db.Model):
     __tablename__ = "users"
 
-    id = Column(BigInteger, primary_key=True)
-    username = Column(String(150), unique=True, nullable=False)
-    password_hash = Column(Text, nullable=False)
-    role = Column(String(50), nullable=False, default="user")
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
+    role = db.Column(db.String(50), nullable=False, default="user")
 
-    def set_password(self,password) :
-        self.password_hash  = generate_password_hash(password)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-    def check_password(self,password) :
-        return check_password_hash(self.password_hash,password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
