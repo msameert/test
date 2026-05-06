bea0a762d67d2956cb67ebbc616d7eefdfd94d88
msameert <msameert123@gmail.com>
2026-05-06 15:45:44 +0500

    faculty updated

--- backend/models/faculty.py
+++ backend/models/faculty.py
@@ -6,11 +6,11 @@ class Faculty(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text,nullable=False)
     email = db.Column(db.Text,nullable=True)
-    course_name = db.Column(db.String(70), nullable=False)
-    course_name2 = db.Column(db.String(70), nullable=True)
     department = db.Column(db.String(100))
     designation = db.Column(db.String(100), nullable=True)
     
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     
     user = db.relationship("User", backref="faculty", uselist=False)
+
+    courses = db.relationship("FacultyCourse", back_populates="faculty")
