4cbd4b5667abf3dc1f6dce00727b5d6d4593c241
msameert <msameert123@gmail.com>
2026-09-20 21:39:42 +0500

    fix(Schema): updating User Schema for error Validations

--- backend/schemas/login_schema.py
+++ backend/schemas/login_schema.py
@@ -5,7 +5,7 @@ class LoginSchema(Schema):
     username = fields.Str(
         required=True,
         validate=[
-            validate.Length(min=4, max=30),
+            validate.Length(min=5, max=30),
             validate.Regexp(
                 r"^(?!.*(?:['\";]|--|/\*|\*/|SELECT|INSERT|UPDATE|DELETE|DROP|ALTER|UNION|OR\s+1\s*=\s*1|EXEC))[A-Za-z0-9_.-@]+$",
                 error="Username contains invalid characters or unsafe SQL patterns."
